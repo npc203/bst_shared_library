b74893c791056faf0ce94284b62b1ba560c71284
19EUCS107_Pranesh S <praneshgowtham005@gmail.com>
2022-04-07 15:42:18 +0530

    print format correction

--- bst/bst.c
+++ bst/bst.c
@@ -12,7 +12,7 @@ struct node *newNode(int item) {
 void inorder(struct node *root) {
   if (root != NULL) {
     inorder(root->left);
-    printf("%d -> ", root->key);
+    printf("%d  ", root->key);
     inorder(root->right);
   }
 }
