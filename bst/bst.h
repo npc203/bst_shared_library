7ceedb0eef536571f2aaec374f58f362142291c9
19EUCS107_Pranesh S <praneshgowtham005@gmail.com>
2022-04-12 17:33:07 +0530

    removed unwanted function in h file

--- bst/bst.h
+++ bst/bst.h
@@ -5,7 +5,6 @@ struct node {
   struct node *left, *right;
 };
 
-struct node *createRoot(int);
 void printTree(struct node*);
 struct node *insert(struct node*, int);
 bool deleteNode(struct node*, int);
